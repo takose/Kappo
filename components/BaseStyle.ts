4a8b97415718d24e94f4c1104f4f59e7b23f0b0f
tako <rina.m.takase@gmail.com>
2019-10-24 00:27:21 +0900

    Create BaseStyle

---
+++ components/BaseStyle.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import { BROWN } from './constants';
+
+export const BaseH1 = styled.h1`
+  color: #252525;
+  font-size: 32px;
+`
+
+export const BaseLink = styled.a`
+  color: ${BROWN};
+  font-size: 14px;
+  text-decoration: none;
+  cursor: pointer;
+`
