af46351acb38c517734911941b932e2a6b3418a5
tako <rina.m.takase@gmail.com>
2019-10-23 23:31:01 +0900

    Not to use sass

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
 
-module.exports = withTypescript(
-  withSass({
-    webpack(config, options) {
-      // Further custom configuration here
-      return config;
-    }
-  })
-);
+module.exports = withTypescript({
+  webpack(config, _) {
+    // Further custom configuration here
+    return config;
+  }
+});
