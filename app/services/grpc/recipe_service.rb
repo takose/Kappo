d3e704110945b610bd6a8f2e719a146c0f979e72
tako <rina.m.takase@gmail.com>
2019-10-27 18:23:56 +0900

    Add recipe_service

---
+++ app/services/grpc/recipe_service.rb
@@ -0,0 +1,18 @@
+class Grpc::RecipeService < Bank::RecipeService::Service
+  def get_by_id(req, _call)
+    recipe = Recipe.find(req.id)
+    if recipe.present?
+      Coot::GetByUserIdResponse.new(recipe: recipe.to_proto)
+    else
+      raise GRPC::BadStatus.new_status_exception(
+        GRPC::Core::StatusCodes::ABORTED,
+        "Wallet(user_id=#{user_id}) isn't found.",
+      )
+    end
+  end
+
+  def create_recipe(req, _call)
+    recipe = Recipe.create(title: req.title)
+    Coot::CreateRecipeResponse.new(recipe: recipe.to_proto)
+  end
+end
