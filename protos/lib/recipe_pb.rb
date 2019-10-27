eb7c05817170b22dc549bb48720d390a1031f89a
tako <rina.m.takase@gmail.com>
2019-10-27 18:09:17 +0900

    Fix package

--- protos/lib/recipe_pb.rb
+++ protos/lib/recipe_pb.rb
@@ -5,29 +5,29 @@ require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
   add_file("recipe.proto", :syntax => :proto3) do
-    add_message "recipe.GetByIdRequest" do
+    add_message "coot.GetByIdRequest" do
       optional :id, :int32, 1
     end
-    add_message "recipe.GetByIdResponse" do
-      optional :recipe, :message, 1, "recipe.Recipe"
+    add_message "coot.GetByIdResponse" do
+      optional :recipe, :message, 1, "coot.Recipe"
     end
-    add_message "recipe.CreateRecipeRequest" do
+    add_message "coot.CreateRecipeRequest" do
       optional :title, :string, 1
     end
-    add_message "recipe.CreateRecipeResponse" do
-      optional :recipe, :message, 1, "recipe.Recipe"
+    add_message "coot.CreateRecipeResponse" do
+      optional :recipe, :message, 1, "coot.Recipe"
     end
-    add_message "recipe.Recipe" do
+    add_message "coot.Recipe" do
       optional :id, :int32, 1
       optional :title, :string, 2
     end
   end
 end
 
-module Recipe
-  GetByIdRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("recipe.GetByIdRequest").msgclass
-  GetByIdResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("recipe.GetByIdResponse").msgclass
-  CreateRecipeRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("recipe.CreateRecipeRequest").msgclass
-  CreateRecipeResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("recipe.CreateRecipeResponse").msgclass
-  Recipe = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("recipe.Recipe").msgclass
+module Coot
+  GetByIdRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("coot.GetByIdRequest").msgclass
+  GetByIdResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("coot.GetByIdResponse").msgclass
+  CreateRecipeRequest = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("coot.CreateRecipeRequest").msgclass
+  CreateRecipeResponse = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("coot.CreateRecipeResponse").msgclass
+  Recipe = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("coot.Recipe").msgclass
 end
