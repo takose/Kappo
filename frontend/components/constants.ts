27748a18e8b3ad472f64f346dd8879be06b35963
tako <rina.m.takase@gmail.com>
2019-10-28 09:13:22 +0900

    Add color constants

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -1,3 +1,5 @@
 export const RED = '#d31d2a';
 export const GREEN = '#c3bf5b';
-export const BROWN = '#473930';
+export const DARK_BROWN = '#473930';
+export const BROWN = '#e1cba2';
+export const LIGHT_BROWN = '#8f6849';
