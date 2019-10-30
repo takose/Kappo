05235da1bcd9f4084271a9786dfed49655a6d5a4
tako <rina.m.takase@gmail.com>
2019-10-30 22:51:55 +0900

    Impl grpc client

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,32 @@
 import Link from 'next/link';
 import Header from '../components/Header';
 
-const Index = () => (
-  <div>
-    <Header />
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
-    <p>Hello Next.js</p>
-  </div>
-);
+import * as grpc from 'grpc';
+import * as recipe_grpc_pb from '../generated/recipe_grpc_pb';
+import * as recipe_pb from '../generated/recipe_pb';
+
+const Index = () => {
+  const client = new recipe_grpc_pb.RecipeServiceClient(
+    'localhost:8080',
+    grpc.credentials.createInsecure(),
+  );
+
+  const req = new recipe_pb.GetByIdRequest();
+  req.setId(1)
+
+  client.getById(req, function (error, result) {
+    if (error) console.log('Error: ', error);
+    else console.log(result.toObject());
+  });
+  return (
+    <div>
+      <Header />
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
+      <p>Hello Next.js</p>
+    </div>
+  )
+};
 
 export default Index;
