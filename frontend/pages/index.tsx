b3ab6d704a9d0537bdfd3acb16cbaee3cec9dca9
tako <rina.m.takase@gmail.com>
2019-11-18 22:05:20 +0900

    Copy Sample

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,43 @@ import * as grpc from 'grpc';
 import * as recipe_grpc_pb from '../generated/recipe_grpc_pb';
 import * as recipe_pb from '../generated/recipe_pb';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import gql from 'graphql-tag';
+
+const gqlClient = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000/',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
 const Index = () => {
+  gqlClient.query({
+    query: gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `
+  }).then(result => console.log(result['data']));
   const client = new recipe_grpc_pb.RecipeServiceClient(
     'localhost:8080',
     grpc.credentials.createInsecure(),
